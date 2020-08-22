00407daf3cadf0c52790e0f841bbd63f1a38b2fd
rodcko2417 <rodcko@live.com>
2020-08-22 17:29:25 -0600

    Que es y como crear tu propio template engine

---
+++ template-engine/express-jsx.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+function getRenderedContent(content, object) {
+
+}
+
+function expressJsx(filePath, options, callback) {
+    fs.readFile(filePath, function(err, content){
+        if (err) {
+            return callback(err);
+        }
+
+        const rendered = getRenderedContent(content, options);
+
+        return callback(null, rendered);
+    });
+}
+
+module.exports = expressJsx;
---
+++ template-engine/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const expressJsx = require('./express-jsx');
+
+app.engine("jsx", expressJsx);
+app.set("views", "./views");
+app.set("view engine", "jsx");
+
+app.get('/', function(req, res) {
+    res.render(index, { hello: "hola", world: "mundo"});
+
+});
+
+const server = app.listen(8000, function() {
+    console.log(`Listening http://localhost:${server.address().port}`);
+});
