0bfdc598f0e9fa2cfb66df104b4fe7f42ad3568e
rodcko2417 <rodcko@live.com>
2020-08-22 18:52:02 -0600

    Implementando un template engine

--- template-engine/express-jsx.js
+++ template-engine/express-jsx.js
@@ -1,7 +1,21 @@
 const fs = require('fs');
 
-function getRenderedContent(content, object) {
+function getKeysFromOptions(options) {
+    const { settings, _locals, ...objectKeys } = options;
+    return Object.keys(objectKeys);
+}
 
+function getRenderedContent(content, options) {
+    const keys = getKeysFromOptions(options);
+    let contentString = content.toString();
+
+    for (let key of keys) {
+        contentString = contentString.replace(
+            new RegExp(`\{${key}\}`, "gi"), 
+            options[key]
+        );
+    }
+    return contentString;
 }
 
 function expressJsx(filePath, options, callback) {
--- template-engine/index.js
+++ template-engine/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const expressJsx = require('./express-jsx');
 
-app.engine("jsx", expressJsx);
-app.set("views", "./views");
-app.set("view engine", "jsx");
+app.engine('jsx', expressJsx);
+app.set('views', './views');
+app.set('view engine', 'jsx');
 
 app.get('/', function(req, res) {
-    res.render(index, { hello: "hola", world: "mundo"});
+    res.render('index', { hello: "hola", world: "mundo genial"});
 
 });
 
