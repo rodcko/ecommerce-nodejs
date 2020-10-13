13df9e0fe382b753505c79f6079c4ef5935d7426
rodcko2417 <rodcko@live.com>
2020-10-13 14:42:33 -0600

    Creando nuestro layout de productos usando Pug

--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const products = [
     {
         name: "Red shoes",
-        price: 75
+        price: 75,
+        image: "https://images.unsplash.com/photo-1590483736622-39da8acf7eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
     },
     {
         name: "Black bike",
-        price: 300
+        price: 300,
+        image: "https://images.unsplash.com/photo-1469534650455-c107956c869d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
     }
 ]
 
