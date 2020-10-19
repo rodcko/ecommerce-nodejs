2b6c1ad0636092b76e3094984cf8e61b314b73a5
rodcko2417 <rodcko@live.com>
2020-10-18 18:51:08 -0600

    Anatomia de una API RESTful e implementacion CRUD

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require("path");
 const app = express();
 const productsRouter = require('./routes/products');
+const productsApiRouter = require('./routes/api/products');
+
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
@@ -9,7 +11,8 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use('/products', productsRouter);
+app.use("/products", productsRouter);
+app.use("/api/products", productsApiRouter);
 
 
 const server = app.listen(8000, function(){
---
+++ ecommerce/utils/mocks/products.js
@@ -0,0 +1,14 @@
+const productMocks = [
+    {
+        name: "Red shoes",
+        price: 75,
+        image: "https://images.unsplash.com/photo-1590483736622-39da8acf7eb8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"
+    },
+    {
+        name: "Black bike",
+        price: 300,
+        image: "https://images.unsplash.com/photo-1469534650455-c107956c869d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    }
+]
+
+module.exports = productMocks;
