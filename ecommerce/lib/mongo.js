f29555f6cd29d95f8a688d7cb02ba181e6d98018
unknown <rodcko@live.com>
2020-11-29 16:42:49 -0600

    Conectando nuestros servicios con mongo DB

---
+++ ecommerce/lib/mongo.js
@@ -0,0 +1,38 @@
+const { MongoClient } = require('mongodb');
+const { config } = require('../config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@cluster0.8fnub.mongodb.net//${DB_NAME}?retryWrites=true&w=majority`;
+
+class MongoLib {
+    constructor() {
+        this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+        this.dbName = DB_NAME;
+    }
+
+    connect() {
+        return new Promise((resolve, reject) => {
+            this.client.connect(error => {
+                if(error) {
+                    reject(error);
+                }
+                console.log('Connected succesfully to mongo');
+                resolve(this.client.db(this.dbName));
+            });
+        });
+    }
+
+    getAll(collection, query) {
+        return this.connect().then(db => {
+            return db
+                .collection(collection)
+                .find(query)
+                .toArray();
+        });
+    }
+}
+
+module.exports = MongoLib;
