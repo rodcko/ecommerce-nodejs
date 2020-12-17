dc55639b94f332bfb738a1f0688ad8b69b2cce74
unknown <rodcko@live.com>
2020-12-16 23:59:14 -0600

    Asegurando nuestra API e identificando rutas sensibles

--- ecommerce/routes/api/products.js
+++ ecommerce/routes/api/products.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const ProductsService = require('../../services/products');
 const validation = require('../../utils/middlewares/validationHandler');
@@ -9,6 +10,9 @@ const {
     createProductSchema, 
     updateProductSchema
 } = require('../../utils/schemas/products');
+
+// JWT strategy
+require('../../utils/auth/strategies/jwt');
 
 const productService = new ProductsService();
 
@@ -67,6 +71,7 @@ router.post('/', validation(createProductSchema), async function(
 
 router.put(
     '/:productId', 
+    passport.authenticate("jwt", { session: false }), // Verifica que este endpoint se devuelva a un user que tenga acceso
     validation({ productId: productIdSchema }, "params"), 
     validation(updateProductSchema), 
     async function(req, res, next) {
@@ -90,7 +95,9 @@ router.put(
     }
 });
 
-router.delete('/:productId', async function(req, res, next) {
+router.delete('/:productId', 
+    passport.authenticate("jwt", { session: false }), // Verifica que este endpoint se devuelva a un user que tenga acceso
+    async function(req, res, next) {
     const { productId } = req.params;
     console.log("req", req.params);
 
